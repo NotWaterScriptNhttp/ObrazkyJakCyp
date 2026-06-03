5739ec748c6fc800d9caf04069381ab611000da2
NotWaterScriptNhttp <JustNotKokot.cc.pub.priv@seznam.cz>
2026-06-04 00:03:59 +0200

    Update PluginConfig.cs

--- ObrazkyJakCyp/PluginConfig.cs
+++ ObrazkyJakCyp/PluginConfig.cs
@@ -1,19 +1,31 @@
 ﻿using System;
-
+using System.IO;
+using BepInEx;
 using BepInEx.Configuration;
 
 namespace ObrazkyJakCyp
 {
     public class PluginConfig
     {
-        public ConfigEntry<string> Directory;
+        public string Directory { get; private set; } = "Paintings";
+        public int MaxImages { get; private set; } = 500;
 
         public PluginConfig(ConfigFile cfg)
         {
-            Directory = cfg.Bind<string>(
+            var dir = cfg.Bind<string>(
                 new ConfigDefinition("General", "Directory"),
-                "Paintings"
+                Directory
             );
+            var maxImgs = cfg.Bind<int>(
+                new ConfigDefinition("General", "Max Images"),
+                MaxImages
+            );
+
+            if (Path.IsPathRooted(dir.Value))
+                Directory = dir.Value;
+            else Directory = Path.GetFullPath(Path.Combine(Paths.BepInExRootPath, dir.Value));
+
+            MaxImages = maxImgs.Value;
         }
     }
 }
